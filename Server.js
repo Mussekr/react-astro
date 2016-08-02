4892310e9833f0ea2020038211ad742cf9acd6a1
Rasmus Kröger <rasmuskroger@gmail.com>
2016-08-03 00:36:23 +0300

    Added api upload form and  handler.

--- Server.js
+++ Server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 var config = {
   user: 'postgres', //env var: PGUSER
@@ -21,21 +24,24 @@ app.get('/api/image/:id', function(req, res) {
 		if(err) {
 			res.json({success: false, error: err});
 		}
-		client.query('SELECT encode(image, 'base64') as image FROM public.images WHERE id = $1', [req.params.id], function (err, result) {
+		client.query('SELECT image FROM public.images WHERE id = $1', [req.params.id], function (err, result) {
 
 			done();
 
 			if(err) {
 				res.json({success: false, error: err});
 			}
-			res.json(result.rows[0].image);
-			console.log(result.rows[0].image);
-			/*var img = new Buffer(result.rows[0], 'base64');
-			res.writeHead(200, {
-				'Content-Type': 'image/png',
-				'Content-Length': img.length
-			});
-			res.end(img);*/
+
+			if(result.rows.length > 0) {
+				var img = new Buffer(result.rows[0].image, 'binary');
+				res.writeHead(200, {
+					'Content-Type': 'image/jpeg',
+					'Content-Length': img.length
+				});
+				res.end(img);
+			} else {
+				res.json({success: false, error: "image not found!"});
+			}
 		});
 	});
 
@@ -44,6 +50,33 @@ app.get('/api/image/:id', function(req, res) {
 
 app.post('/api/login', function(req, res) {
 	res.json({success: true});
+});
+
+app.post('/api/upload', upload.single('image'), function(req, res) {
+	console.log(req.file.buffer);
+	console.log(req.body.name);
+	pool.connect(function(err, client, done) {
+		if(err) {
+			res.json({success: false});
+		}
+		client.query('INSERT INTO images (name, image) VALUES ($1, $2)', [req.body.name], [req.file.buffer], function(err, result) {
+			done();
+			if(err) {
+				res.json({success: false});
+			}
+			if(result.rows.length > 0) {
+				res.json({success: true});
+			} else {
+				res.json({success: false});
+			}
+		});
+	});
+});
+
+//placeholder form
+var content = '<html><body><form method="post" enctype="multipart/form-data" action="/api/upload"><p><input type="text" name="name" placeholder="Name" /></p><p><input type="file" name="image" /></p><p><input type="submit" /></body></html>';
+app.get('/api/upload', function(req, res) {
+	res.send(content);
 });
 
 app.listen(process.env.PORT || 8080);
