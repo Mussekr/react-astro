d978d99c7467df558fe9ea53ed0cfed2ef0a0024
Rasmus Kröger <rasmuskroger@gmail.com>
2016-08-03 11:01:00 +0300

    Environment variables

--- Server.js
+++ Server.js
@@ -6,14 +6,9 @@ var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
 var config = {
-  user: 'postgres', //env var: PGUSER
-  database: 'astrogallery', //env var: PGDATABASE
-  password: 'root', //env var: PGPASSWORD
-  port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
-
 var pool =  new pg.Pool(config);
 
 app.use(express.static(__dirname + '/'));
