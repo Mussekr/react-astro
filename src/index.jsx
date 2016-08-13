c6c8e142ab02887cdf49a376c84baa83d5cf6f0b
Pyry Kröger <pyry@krogerit.net>
2016-08-13 13:06:33 +0300

    remove commented out code

--- src/index.jsx
+++ src/index.jsx
@@ -15,30 +15,6 @@ var FormControl = require('react-bootstrap').FormControl;
 var Button = require('react-bootstrap').Button;
 var Collapse = require('react-bootstrap').Collapse;
 
-/*
-var Hello = React.createClass({
-  displayName: 'Hello',
-  getInitialState: function() {
-    return {greeting: 'Hello'};
-  },
-  toggleGreeting: function() {
-    this.setState({greeting: this.state.greeting === 'Hello' ? 'Howdy' : 'Hello'});
-  },
-  render: function() {
-    return (
-      <div>
-        {this.state.greeting} {this.props.value}
-        <button onClick={this.toggleGreeting}>Toggle greeting</button>
-      </div>
-    );
-  }
-});
-
-ReactDOM.render(
-  <Hello value="World" />,
-  document.getElementById('container')
-);*/
-
 var LoginForm = React.createClass({
   render: function() {
     return (
