afaaef582532f064cf80ad5be29417ebb093c01d
Mussekr <rasmuskroger@gmail.com>
2016-08-19 20:15:19 +0300

    updated query

--- migrations/sqls/20160819150446-users-categories-up.sql
+++ migrations/sqls/20160819150446-users-categories-up.sql
@@ -7,10 +7,10 @@ CREATE TABLE categories (
   "image" bigint,
   PRIMARY KEY ("id")
 )
-INSERT INTO categories (id, name, image) VALUES (1, "Nebulae", 3);
-INSERT INTO categories (id, name, image) VALUES (2, "Galaxies", 4);
-INSERT INTO categories (id, name, image) VALUES (3, "Best of", 6);
+INSERT INTO categories ("id", "name", "image") VALUES (1, "Nebulae", 3);
+INSERT INTO categories ("id", "name", "image") VALUES (2, "Galaxies", 4);
+INSERT INTO categories ("id", "name", "image") VALUES (3, "Best of", 6);
 
 UPDATE images SET userid = 1;
 
-/*INSERT INTO users (id, username, password, role) VALUES (1, "Musse", "$2a$10$6BL3hWTTWU5Coi.dmZuBmuSaFHlbpTrzS2BvbaLHcNiyaEuqgClbS", 1);*/
+INSERT INTO users ("id", "username", "password", "role") VALUES (1, "Musse", "$2a$10$6BL3hWTTWU5Coi.dmZuBmuSaFHlbpTrzS2BvbaLHcNiyaEuqgClbS", 1);
