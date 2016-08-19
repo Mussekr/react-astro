584d132d23d05bdfd54ac53d8eca4e214810f460
Mussekr <rasmuskroger@gmail.com>
2016-08-19 19:30:00 +0300

    updated query

--- migrations/sqls/20160819150446-users-categories-up.sql
+++ migrations/sqls/20160819150446-users-categories-up.sql
@@ -13,4 +13,4 @@ INSERT INTO categories (id, name, image) VALUES (3, "Best of", 6);
 
 UPDATE images SET userid = 1;
 
-INSERT INTO users (id, username, password, role) VALUES (1, "Musse", "$2a$10$6BL3hWTTWU5Coi.dmZuBmuSaFHlbpTrzS2BvbaLHcNiyaEuqgClbS", 1);
+/*INSERT INTO users (id, username, password, role) VALUES (1, "Musse", "$2a$10$6BL3hWTTWU5Coi.dmZuBmuSaFHlbpTrzS2BvbaLHcNiyaEuqgClbS", 1);*/
